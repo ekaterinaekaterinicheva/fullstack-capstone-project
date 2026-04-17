ebe0756f2fce6cd15a398bbc27c153c44293e4e2
Ekaterina <1ekaterina.kovaleva@gmail.com>
2026-04-17 09:50:47 -0400

    Implement API call for handleLogin()

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,48 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 import './LoginPage.css';
 
 function LoginPage() {
-
-    // The useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState(''); // A state for incorrect password
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
 
-    // The handleLogin function
-    const handleLogin = async () => {
-        console.log("Inside handleLogin");
-    }
+    // If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
-        return (
-      <div className="container mt-5">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <div className="login-card p-4 border rounded">
-              <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        // Api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        });
 
-          {/* Input elements for the variables email and  password */}
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input
-            id="email"
-            type="text"
-            className="form-control"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+        // Access data coming from fetch API
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            // Set user details
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            // Set the user's state to log in
+          setIsLoggedIn(true);
+            // Navigate to the MainPage after logging in.
+          navigate('/app');
+        } else {
+            // Clear input and set an error message if the password is incorrect
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
 
-          {/* The bbutton that performs the `handleLogin` function on click */}
-          <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
-                <p className="mt-4 text-center">
-                    New here? <a href="/app/register" className="text-primary">Register Here</a>
-                </p>
+      }
 
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <div className="login-card p-4 border rounded">
+                        <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                        {/* Email */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
+                            />
+                        </div>
+
+                        {/* Password */}
+                        <div className="mb-4">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
+                            />
+
+                            {/* Display an error message to the user */}
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+                        </div>
+                        <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+                        <p className="mt-4 text-center">
+                            New here? <a href="/app/register" className="text-primary">Register Here</a>
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    )
+    );
 }
 
 export default LoginPage;
