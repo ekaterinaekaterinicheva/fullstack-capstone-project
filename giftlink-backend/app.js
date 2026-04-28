cc2cafda8bdc09f1f3439eda4a3e7f56e875ddbd
ekaterinaekaterinicheva <1ekaterina.kovaleva@gmail.com>
2026-04-28 15:30:04 +0200

    Update app.use(cors())

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -9,7 +9,7 @@ const {loadData} = require("./util/import-mongo/index");
 
 
 const app = express();
-app.use("*",cors());
+app.use(cors());
 const port = process.env.PORT || 3060;
 
 // Connect to MongoDB
